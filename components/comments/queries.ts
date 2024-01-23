69d7a463ebcf089af98c7a61f12932c30505fc19
Matt Dean <mdean400@gmail.com>
2024-01-23 00:29:37 -0700

    basic working drizzle-ish query

--- components/comments/queries.ts
+++ components/comments/queries.ts
@@ -1,5 +1,5 @@
 import { commentsTable, db, usersTable } from "@/app/db";
-import { sql } from "drizzle-orm";
+import { max, sql } from "drizzle-orm";
 
 const PER_PAGE = 50;
 
@@ -23,61 +23,93 @@ export async function getComments({
   // using two distinct queries: one for story and
   // one for author, including the max_level_comments
   // part only for the author query.
-  // Define the recursive part of the query using raw SQL
-  const recursivePart = sql`
+  const withThing = sql`
     WITH RECURSIVE dfs_comments AS (
-      SELECT 
+      SELECT
         comments.*,
         ARRAY[comments.created_at] AS path,
         1 AS level
-      FROM 
+      FROM
         comments
-      WHERE 
+      WHERE
         (${sId} <> '' AND comments.story_id = ${sId} AND comments.parent_id IS NULL)
-        OR 
+        OR
         (${aId} <> '' AND comments.author = ${aId})
-      
+
       UNION ALL
 
-      SELECT 
+      SELECT
         comments.*,
         path || comments.created_at,
         level + 1
-      FROM 
+      FROM
         comments
-      JOIN 
+      JOIN
         dfs_comments ON comments.parent_id = dfs_comments.id
-    )`;
-
-  // Continue building the rest of the query using Drizzle ORM
-  const result = db
-    .select({
-      id: sql<string>`dfs_comments.id`,
-      story_id: sql<string>`dfs_comments.story_id`,
-      ancestor_id: sql<string>`dfs_comments.ancestor_id`,
-      username: sql<string>`dfs_comments.author_username`,
-      author_username: usersTable.username,
-      comment: sql<string>`dfs_comments.comment`,
-      parent_id: sql<string>`dfs_comments.parent_id`,
-      author: sql<string>`dfs_comments.author`,
-      created_at: sql<Date>`dfs_comments.created_at`,
-      updated_at: sql<Date>`dfs_comments.updated_at`,
-      level: sql<number>`dfs_comments.level`,
-    })
-    .from(recursivePart) // Use the raw SQL as part of the query
-    .innerJoin(
-      sql`max_level_comments`,
-      sql`dfs_comments.id = max_level_comments.id`
+    ), `
+    .append(sql`max_level_comments AS (`)
+    .append(
+      db
+        .select({
+          id: sql`id`,
+          max_level: sql`MAX(level)`.as("max_level"),
+        })
+        .from(sql`dfs_comments`)
+        .groupBy(sql`id`)
+        .getSQL()
     )
-    .where(sql`dfs_comments.level = max_level_comments.max_level`)
-    .leftJoin(usersTable, sql`${usersTable.id} = dfs_comments.author`)
-    .orderBy(sql`path`)
-    .offset((page - 1) * PER_PAGE)
-    .limit(PER_PAGE);
+    .append(sql`) `)
+    .append(
+      db
+        .select({
+          id: sql<string>`dfs_comments.id`,
+          story_id: sql<string>`dfs_comments.story_id`,
+          // ancestor_id: sql<string>`dfs_comments.ancestor_id`,
+          username: sql<string>`dfs_comments.username`,
+          author_username: usersTable.username,
+          comment: sql<string>`dfs_comments.comment`,
+          parent_id: sql<string>`dfs_comments.parent_id`,
+          author: sql<string>`dfs_comments.author`,
+          created_at: sql<string>`dfs_comments.created_at`,
+          updated_at: sql<string>`dfs_comments.updated_at`,
+          level: sql<number>`dfs_comments.level`,
+        })
+        .from(sql`dfs_comments`)
+        .innerJoin(
+          sql`max_level_comments`,
+          sql`dfs_comments.id = max_level_comments.id`
+        )
+        .where(sql`dfs_comments.level = max_level_comments.max_level`)
+        .leftJoin(usersTable, sql`${usersTable.id} = dfs_comments.author`)
+        .orderBy(sql`path`)
+        .offset((page - 1) * PER_PAGE)
+        .limit(PER_PAGE)
+        .getSQL()
+    );
 
-  return result;
+  const result = await db.execute(withThing);
+
+  // console.debug("result", result.rows);
+
+  const row1 = result.rows[0];
+  console.debug("created at", typeof row1.created_at);
+
+  return result.rows;
 }
-export type Comment = Awaited<ReturnType<typeof getComments>>[number];
+
+// TODO: infer this
+export type Comment = {
+  id: string;
+  story_id: string;
+  author_username: string | null;
+  username: string | null;
+  comment: string;
+  parent_id: string | null;
+  author: null;
+  created_at: Date;
+  updated_at: Date;
+  level: number;
+};
 
 export async function hasMoreComments({
   storyId,
